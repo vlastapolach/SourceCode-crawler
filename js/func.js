8e68d7c09f6597d48f6aee4c51b9900909dd1a0f
vlastapolach <vlastapolach@protonmail.com>
2017-07-29 22:20:09 +0200

    Add form validation

--- js/func.js
+++ js/func.js
@@ -3,6 +3,24 @@ var redirectData = [];
 var domNew = "";
 var j = 0;
 var total;
+var oldVal = "";
+var newVal = "";
+
+function enableBtn() {
+  oldVal = $('#oldUrl').val();
+  newVal = $('#newUrl').val();
+  if (oldVal !== "" && newVal !== "") {
+    $("#proceed").removeClass("disabled");
+    $("#proceed").removeClass("btn-danger");
+    $("#proceed").addClass("btn-primary");
+  } else {
+    $("#proceed").addClass("disabled");
+    $("#proceed").removeClass("btn-primary");
+    $("#proceed").addClass("btn-danger");
+  }
+}
+$('#oldUrl').keyup(enableBtn);
+$('#newUrl').keyup(enableBtn);
 
 function subStr() {
   subString = $('#oldUrl').val();
@@ -24,87 +42,88 @@ function getSourceAsDOM(url) {
 }
 
 // Run the searching with button click
-$("#proceed").one("click", function() {
-  event.preventDefault();
-  // read input values
-  subString = $('#oldUrl').val();
-  var newUrla = $("#newUrl").val();
-  var download = $("#download").val();
-  var textbox = document.getElementById('logRes');
-
-  // read URLs and line by line save them as an object
-  var newLines = $('#newUrl').val().split(/\n/);
-  var newUrlResult = [];
-
-  textbox.value += " Total count: " + newLines.length + " URLs \n";
-  textbox.scrollTop = textbox.scrollHeight;
-  total = newLines.length;
-
-  for (var num = 0; num < newLines.length; num++) {
-    setTimeout(function(x) {
-      return function() {
-        var newUrlString = newLines[j];
-        getSourceAsDOM(newLines[j]);
-        var isThere = domNew.search(subString);
-        // If found, save the search phrase
-        if (isThere !== -1) {
-          redirectData.push({
-            NewURL: newLines[j],
-            SearchSubstring: subString
-          });
-          textbox.value += "💚 " + (j + 1) + ": " + newLines[j] + ' - phrase "' + subString + '" was found\n';
-          textbox.scrollTop = textbox.scrollHeight;
-          j++;
-          if (j === total) {
-            completeFnc()
-          }
-        }
-        // If URL doesn't work/exist, save "URL Error"
-        else if (domNew == "") {
-          redirectData.push({
-            NewURL: newLines[j],
-            SearchSubstring: "URL Error"
-          });
-          textbox.value += "⛔ " + (j + 1) + ": " + newLines[j] + " - URL Error\n";
-          textbox.scrollTop = textbox.scrollHeight;
-          j++;
-          if (j === total) {
-            completeFnc()
-          }
-        }
-        // If not found, save "Not Found"
-        else {
-          redirectData.push({
-            NewURL: newLines[j],
-            SearchSubstring: "Not Found"
-          });
-          textbox.value += "👻 " + (j + 1) + ": " + newLines[j] + " - phrase not found\n";
-          textbox.scrollTop = textbox.scrollHeight;
-          j++;
-          if (j === total) {
-            completeFnc()
-          }
-        }
-      } //return function
-    }(num), 0 + num * 200) //timeout
-  }
-
-  var completeFnc = function() {
-    textbox.value += '*** COMPLETED *** \n';
-    textbox.scrollTop = textbox.scrollHeight;
-    // Enable download button
-    $("#download").removeClass("disabled");
-    $("#download").removeClass("btn-danger");
-    $("#download").addClass("btn-primary");
-  }
-
-  // Disable proceed button
-  $("#proceed").addClass("disabled");
-  $("#proceed").addClass("btn-danger");
-});
-
 $("#proceed").click(function() {
   event.preventDefault();
+
+  if ($("#proceed").hasClass("disabled")) {
+    console.log("Not possible - fill in Search phrase and List of URLs.")
+  } else {
+    // read input values
+    subString = $('#oldUrl').val();
+    var newUrla = $("#newUrl").val();
+    var download = $("#download").val();
+    var textbox = document.getElementById('logRes');
+
+    // read URLs and line by line save them as an object
+    var newLines = $('#newUrl').val().split(/\n/);
+    var newUrlResult = [];
+
+    textbox.value += " Total count: " + newLines.length + " URLs \n";
+    textbox.scrollTop = textbox.scrollHeight;
+    total = newLines.length;
+
+    for (var num = 0; num < newLines.length; num++) {
+      setTimeout(function(x) {
+        return function() {
+          var newUrlString = newLines[j];
+          getSourceAsDOM(newLines[j]);
+          var isThere = domNew.search(subString);
+          // If found, save the search phrase
+          if (isThere !== -1) {
+            redirectData.push({
+              NewURL: newLines[j],
+              SearchSubstring: subString
+            });
+            textbox.value += "💚 " + (j + 1) + ": " + newLines[j] + ' - phrase "' + subString + '" was found\n';
+            textbox.scrollTop = textbox.scrollHeight;
+            j++;
+            if (j === total) {
+              completeFnc()
+            }
+          }
+          // If URL doesn't work/exist, save "URL Error"
+          else if (domNew == "") {
+            redirectData.push({
+              NewURL: newLines[j],
+              SearchSubstring: "URL Error"
+            });
+            textbox.value += "⛔ " + (j + 1) + ": " + newLines[j] + " - URL Error\n";
+            textbox.scrollTop = textbox.scrollHeight;
+            j++;
+            if (j === total) {
+              completeFnc()
+            }
+          }
+          // If not found, save "Not Found"
+          else {
+            redirectData.push({
+              NewURL: newLines[j],
+              SearchSubstring: "Not Found"
+            });
+            textbox.value += "👻 " + (j + 1) + ": " + newLines[j] + " - phrase not found\n";
+            textbox.scrollTop = textbox.scrollHeight;
+            j++;
+            if (j === total) {
+              completeFnc()
+            }
+          }
+        } //return function
+      }(num), 0 + num * 200) //timeout
+    }
+
+    var completeFnc = function() {
+      textbox.value += '*** COMPLETED *** \n';
+      textbox.scrollTop = textbox.scrollHeight;
+      // Enable download button
+      $("#download").removeClass("disabled");
+      $("#download").removeClass("btn-danger");
+      $("#download").addClass("btn-primary");
+    }
+
+    // Disable proceed button
+    $("#proceed").addClass("disabled");
+    $("#proceed").addClass("btn-danger");
+  }
 });
 
 /* Download as CSV script from https://halistechnology.com/2015/05/28/use-javascript-to-export-your-data-as-csv/ */
